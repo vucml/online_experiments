e09016730d5bc375bc05447f37a35bdb8a7febb4
Jordan Gunn <gunnjordanb@gmail.com>
2025-02-12 19:15:02 -0600

    Add Prolific ID handling in experiment initialization

--- core/init_experiment.js
+++ core/init_experiment.js
@@ -1,19 +1,24 @@
 function initializeExperiment() {
-  // Prepare jsPsych initialization options
-  let jsPsychInitOptions = {};
-
-  if (typeof jatos !== 'undefined') {
-      console.log('Running in JATOS');
-      jsPsychInitOptions = {
-          on_finish: () => jatos.endStudy(jsPsych.data.get().json())
-      };
-  } else {
-      console.log('Running outside JATOS');
-      jsPsychInitOptions = {
-          on_finish: () => jsPsych.data.displayData()
-      };
+    // Prepare jsPsych initialization options
+    let jsPsychInitOptions = {};
+    let prolific_id = '';
+  
+    if (typeof jatos !== 'undefined') {
+        console.log('Running in JATOS');
+        prolific_id = jatos.urlQueryParameters.PROLIFIC_ID || '';
+        jsPsychInitOptions = {
+            on_finish: (jsPsychInstance) => jatos.endStudy(jsPsychInstance.data.get().json())
+        };
+    } else {
+        console.log('Running outside JATOS');
+        prolific_id = new URLSearchParams(window.location.search).get('PROLIFIC_ID') || '';
+        jsPsychInitOptions = {
+            on_finish: (jsPsychInstance) => jsPsychInstance.data.displayData()
+        };
+    }
+  
+    console.log('PROLIFIC_ID:', prolific_id);
+    const jsPsych = initJsPsych(jsPsychInitOptions);
+    jsPsych.data.addProperties({ prolific_id });
+    return jsPsych;
   }
-
-  // return jsPsych object
-  return initJsPsych(jsPsychInitOptions);
-}
